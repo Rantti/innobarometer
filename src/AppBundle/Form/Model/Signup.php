797351912aa4f4e1a5e4c1012d269a36250076ac
Rantti <antti.o.eloranta@gmail.com>
2015-09-28 16:54:24 +0300

    Entity edits and form classess. Added also a view and a controller.

---
+++ src/AppBundle/Form/Model/Signup.php
@@ -0,0 +1,28 @@
+<?php 
+namespace AppBundle\Form\Model;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+use AppBundle\Entity\User;
+
+class Signup
+{
+    /**
+     * @Assert\Type(type="AppBundle\Entity\User")
+     * @Assert\Valid()
+     */
+    protected $user;
+
+    
+
+    public function setUser(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
+?>
