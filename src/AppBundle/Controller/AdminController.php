2a08353e47595f64cd95ad457561436bdd3e1e08
Rantti <antti.o.eloranta@gmail.com>
2015-11-30 20:01:11 +0200

    Teams and projects are editable now. Also added flashbags to alert user for succesful actions.

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -44,7 +44,35 @@ class AdminController extends Controller
         throw new NotFoundHttpException('User not found for id ' . $id);
       }
       $userManager->deleteUser($user);
+      $this->get('session')->getFlashBag()->add(
+            'notice',
+            'User was removed succesfully.'
+        );
       return new RedirectResponse("users");
+    }
+
+    /**
+     * @Route("/removeteam", name="removeteam",
+     * requirements = { "id" = "\d+" },
+     * methods = { "GET" })
+     *
+     */
+    public function teamRemoveAction(Request $request){
+      $id = $this->getRequest()->get('id');
+      $team = $this->getDoctrine()->getRepository('AppBundle:Team')->find($id);
+      $members = $team->getMembers();
+      foreach ($members as $member) {
+        $member->getUser()->removeTeam($member);
+        $em->remove($member);
+
+      }
+      $em->remove($team);
+      $em->flush;
+      $this->get('session')->getFlashBag()->add(
+            'notice',
+            'Team was removed succesfully!'
+        );
+      return new RedirectResponse("teams");
     }
 
 
@@ -52,7 +80,6 @@ class AdminController extends Controller
      * @Route("/teams", name="teams")
      */
     public function teamAction(Request $request){
-        //$form = $this->createForm(new TeamType(), $post);
       $em = $this->getDoctrine()->getManager();
       $teams = $em->getRepository('AppBundle:Team')->findAll();
       return $this->render('admin/teams.html.twig', array(
@@ -81,7 +108,14 @@ class AdminController extends Controller
           $user->addTeam($member);
         }
         $em->persist($team);
+
+        $team->setInviteToken(substr(uniqid(),0,6));
+
         $em->flush();
+        $this->get('session')->getFlashBag()->add(
+            'notice',
+            'New team was created succesfully!'
+        );
         return $this->redirectToRoute('teams');
       }
       return $this->render('admin/teams_new.html.twig', array(
@@ -98,13 +132,14 @@ class AdminController extends Controller
   */
     public function editAction(Team $team, Request $request)
     {
-
+      
       $em = $this->getDoctrine()->getManager();
 
-      $teamForm = $this->createFormBuilder($team)
+      $teamForm = $this->get("form.factory")->createNamedBuilder("teamForm", "form", $team)
       ->add('teamName', 'text', array('label' => 'label.teamName'))
       ->add('country', 'choice', array('choices' => array('FIN' => 'Finland', 'EST' => 'Estonia', 'NOR' => 'Norway', 'RU' => 'Russia', 'SWE' => 'Sweden'), 
         'required' => true,))
+      ->add('save', 'submit', array('label' => 'Save Changes'))
       ->getForm();
 
 
@@ -115,36 +150,54 @@ class AdminController extends Controller
     }
     $allUsers = $this->getDoctrine()->getRepository('AppBundle:User')->findAll();
     $choiceArray = array_diff($allUsers, $users);
-    $memberForm = $this->createFormBuilder($team)
-
-    ->add('users', 'entity', array(
-            'class' => 'AppBundle:User',
-            'choice_label' => 'username',
-            'property' => 'user',
-            'multiple' => 'true',
-            'expanded' => 'true'
-            ))
-
-
+    $memberForm = $this->get("form.factory")->createNamedBuilder("memberForm")
+    ->add('user', 'entity', array(
+      'class' => 'AppBundle:User',
+      'property' => 'username',
+      'choices' => $choiceArray,
+      ))
     ->add('save', 'submit', array('label' => 'Add Member'))
     ->getForm();
 
-      $teamForm->handleRequest($request);
-
-
-      if ($teamForm->isSubmitted() && $teamForm->isValid()) {
+    if('POST' === $request->getMethod()) {
+ 
+        if ($request->request->has('memberForm')) {
+        $memberForm->handleRequest($request);
+        $newMember = new TeamMember();
+        $newUser = $memberForm['user']->getData();
+        $newMember->setUser($newUser);
+        $newMember->setTeam($team);
+        $newMember->setRole("user");
+        $newUser->addTeam($newMember);
+        $team->addMember($newMember);
+        $em->persist($newMember);
         $em->flush();
-        return $this->render('admin/team/team_edit.html.twig', array(
-        'team'    => $team,
-        'teamForm'    => $teamForm->createView()
-        ));
+        $this->get('session')->getFlashBag()->add(
+            'notice',
+            'New member was added succesfully!'
+        );
         }
+ 
+        if ($request->request->has('teamForm')) {
+        $teamForm->handleRequest($request);  
+        $em->flush();
+        $this->get('session')->getFlashBag()->add(
+            'notice',
+            'Your changes were saved!'
+        );
+        }
+    }
 
       return $this->render('admin/team/team_edit.html.twig', array(
         'team'    => $team,
-        'teamForm'    => $teamForm->createView()
+        'teamForm'    => $teamForm->createView(),
+        'memberForm' => $memberForm->createView()
         ));
+    
+
     }
+
+
     /**
      * @Route("/team/removemember/{id}", name="removemember",
      * requirements = { "id" = "\d+" },
@@ -159,6 +212,10 @@ class AdminController extends Controller
       $user->removeTeam($member);
       $em->remove($member);
       $em->flush();
+      $this->get('session')->getFlashBag()->add(
+            'notice',
+            'User was removed succesfully!'
+        );
       return $this->redirectToRoute('team_edit', array(
         'id' => $team->getId()));
     }
